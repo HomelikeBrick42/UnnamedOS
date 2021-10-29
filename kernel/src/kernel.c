a29459e5151027c2ecf9e5d0f5c68ab9b84a7b6f
HomelikeBrick42 <lukevercoe555@gmail.com>
2021-10-29 21:31:01 +1300

    Added check for bounds of framebuffer in PutPixel()

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -24,6 +24,10 @@ typedef struct PSF1_Font {
 } PSF1_Font;
 
 void PutPixel(Framebuffer* framebuffer, uint32_t x, uint32_t y, uint32_t color) {
+	if (x > framebuffer.Width || y > framebuffer.Height) {
+		return;
+	}
+
 	const uint32_t BBP = 4;
 	*(uint32_t*)(x * BBP + (y * framebuffer->PixelsPerScanLine * BBP) + framebuffer->BaseAddress) = color;
 }
